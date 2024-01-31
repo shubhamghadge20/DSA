1ca0b15f3ef3b058856846ed760e1db61ff1ce36
Shubham Ghadge <shubhamghadge784@gmail.com>
2024-01-31 13:40:07 +0530

    array input

--- Array/basic.java
+++ Array/basic.java
@@ -3,6 +3,7 @@ package Array;
 public class basic {
     //syntax
     //datatype[] variable_name = new datatype[size];
-    //
+    
+    String[] arr = new String[4];
     
 }
---
+++ Array/input.java
@@ -0,0 +1,20 @@
+package Array;
+
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        
+        //input using  for loop
+        for(int i=0; i<arr.length;i++){
+            arr[i] =sc.nextInt();
+        }
+
+        for(int i=0; i<arr.length; i++){
+         System.out.println(arr[i]);   
+        }
+    }
+    
+}
