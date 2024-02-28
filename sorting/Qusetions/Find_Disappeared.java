c4722b24a2f18e8a30d85c405ad8c6bc6a2b72cc
Shubham Ghadge <shubhamghadge784@gmail.com>
2024-02-28 21:40:08 +0530

    cyclic sort question

---
+++ sorting/Qusetions/Find_Disappeared.java
@@ -0,0 +1,44 @@
+ package sorting.Qusetions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Find_Disappeared {
+    public static void main(String[] args) {
+        int nums [] ={4,3,2,7,8,2,3,1};
+        Find_Disappeared solution = new Find_Disappeared();
+        List<Integer> result = solution.findDisappearedNumbers(nums);
+        System.out.println(result);
+       
+    }
+  
+      public List<Integer> findDisappearedNumbers(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i , correct);
+            } else {
+                i++;
+            }
+        }
+
+        // just find missing numbers
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index+1) {
+                ans.add(index + 1);
+            }
+        }
+
+        return ans;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+    
+}
