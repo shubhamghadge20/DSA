b80223a345919dbb4c996e2661803715ef758a4d
Shubham Ghadge <shubhamghadge784@gmail.com>
2024-03-05 14:43:37 +0530

    String and String builder

---
+++ Strings/Operators.java
@@ -0,0 +1,21 @@
+package Strings;
+
+import java.util.ArrayList;
+
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println('a' + 'b');
+        System.out.println("a" + "b");
+        System.out.println((char)('a' + 3));
+
+        System.out.println("a" +1);
+        //integer will be converted to integer that will call toString()
+
+        System.out.println("shubh" + new ArrayList<>());
+        System.out.println("shubh" + new Integer(56));
+        String ans = new Integer(56)+ ""+new ArrayList<>();
+        System.out.println(ans);
+        System.out.println("a" +'b');
+    }
+    
+}
---
+++ Strings/preetyPrinting.java
@@ -0,0 +1,17 @@
+package Strings;
+
+
+
+public class preetyPrinting {
+    public static void main(String[] args) {
+        float a = 453.1274f;
+//        System.out.printf("Formatted number is %.2f", a);
+
+//        System.out.printf("Pie: %.3f", Math.PI);
+
+        System.out.printf("Hello my name is %s and I am %s", "Kunal", "Cool");
+    }
+}
+ 
+    
+
